a51d864fb5eb8472306155a793ce0e24e959bba9
ammarbagasrawala <109103869+ammarbagasrawala@users.noreply.github.com>
2022-12-29 21:25:45 +0530

    Update 1_python.py

--- 1_python.py
+++ 1_python.py
@@ -39,8 +39,7 @@ nextgame=[]
 creepy_doll = ['red_light', 'green_light', 'red_light', 'you_got_shot', 'green_light', 'you_got_shot',
 'you_got_shot', 'green_light', 'you_ got_shot', 'red_light', 'green_light', 'red_light', 'you_got_shot',
 'green_light','red_light', 'red_light', 'green_light', 'you_got_shot','red_light', 'you_got_shot']
-for green in creepy_doll:
-    if green == 'green_light':
-        nextgame.append(green)
+
+nextgame=creepy_doll[1:-1:3]
 
 print(nextgame)
